9a5d32c2a0981e1afcadc7bb28d2708e202afe0d
Abdelrahman Awad <logaretm1@gmail.com>
2020-02-04 22:30:06 +0200

    test: added tests for memory status

---
+++ test/Memory.spec.ts
@@ -0,0 +1,93 @@
+import { mountHook } from './helpers';
+import { useMemoryStatus } from '../src';
+
+const getMemoryStatus = (vm: any) => ({
+  unsupported: false,
+  deviceMemory: vm.deviceMemory,
+  totalJSHeapSize: vm.totalJSHeapSize,
+  usedJSHeapSize: vm.usedJSHeapSize,
+  jsHeapSizeLimit: vm.jsHeapSizeLimit
+});
+
+describe('useMemoryStatus', () => {
+  test(`should return "true" for unsupported case`, () => {
+    const vm = mountHook(() => useMemoryStatus());
+
+    expect(vm.unsupported).toBe(true);
+  });
+
+  test('should return initialMemoryStatus for unsupported case', () => {
+    const deviceMemory = 4;
+    (global as any).navigator.deviceMemory = deviceMemory;
+
+    const vm = mountHook(() => useMemoryStatus());
+
+    expect(vm.unsupported).toBe(false);
+    expect(vm.deviceMemory).toEqual(deviceMemory);
+  });
+
+  test('should return memory status', () => {
+    const mockMemoryStatus = {
+      deviceMemory: 4,
+      totalJSHeapSize: 60,
+      usedJSHeapSize: 40,
+      jsHeapSizeLimit: 50
+    };
+
+    (global as any).navigator.deviceMemory = mockMemoryStatus.deviceMemory;
+    (global as any).window.performance.memory = {
+      totalJSHeapSize: mockMemoryStatus.totalJSHeapSize,
+      usedJSHeapSize: mockMemoryStatus.usedJSHeapSize,
+      jsHeapSizeLimit: mockMemoryStatus.jsHeapSizeLimit
+    };
+
+    const vm = mountHook(() => useMemoryStatus());
+
+    expect(getMemoryStatus(vm)).toMatchObject({
+      ...mockMemoryStatus,
+      unsupported: false
+    });
+  });
+
+  test('should return memory status without performance memory data', () => {
+    const mockMemoryStatus = {
+      deviceMemory: 4
+    };
+
+    (global as any).navigator.deviceMemory = mockMemoryStatus.deviceMemory;
+    delete (global as any).window.performance.memory;
+
+    const vm = mountHook(() => useMemoryStatus());
+
+    expect(vm.deviceMemory).toEqual(mockMemoryStatus.deviceMemory);
+    expect(vm.unsupported).toEqual(false);
+  });
+
+  test('should not return initialMemoryStatus for supported case', () => {
+    const mockMemoryStatus = {
+      deviceMemory: 4,
+      totalJSHeapSize: 60,
+      usedJSHeapSize: 40,
+      jsHeapSizeLimit: 50
+    };
+
+    // const mockInitialMemoryStatus = {
+    //   deviceMemory: 4
+    // };
+
+    (global as any).navigator.deviceMemory = mockMemoryStatus.deviceMemory;
+
+    (global as any).window.performance.memory = {
+      totalJSHeapSize: mockMemoryStatus.totalJSHeapSize,
+      usedJSHeapSize: mockMemoryStatus.usedJSHeapSize,
+      jsHeapSizeLimit: mockMemoryStatus.jsHeapSizeLimit
+    };
+
+    const vm = mountHook(() => useMemoryStatus());
+
+    expect(getMemoryStatus(vm)).toMatchObject({
+      ...mockMemoryStatus,
+      unsupported: false
+    });
+  });
+});
---
+++ test/helpers.ts
@@ -0,0 +1,40 @@
+import { createApp, compile } from 'vue';
+
+export function mount(component: Record<string, any>) {
+  const app = createApp(component);
+  app.config.devtools = false;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  app.config.warnHandler = () => {};
+  app.config.errorHandler = err => {
+    if (err.message === 'data is not defined') {
+      return;
+    }
+
+    // eslint-disable-next-line no-console
+    console.error(err);
+  };
+
+  document.body.innerHTML = `<div id="app"></div>`;
+  component.render = compile(component.template);
+  component.template = undefined;
+
+  return app.mount('#app');
+}
+
+/**
+ * Takes a hook fn and exposes its values for direct access.
+ *
+ * @param fn The function to unwrap its API
+ */
+export function mountHook(fn: () => Record<string, any>) {
+  const component = {
+    setup() {
+      const api = fn();
+
+      return api;
+    },
+    template: '<div></div>'
+  };
+
+  return mount(component) as any;
+}
